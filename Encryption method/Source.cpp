bc333b844a97f6a9f9c2858705012d035486a693
AvishayDev <62137674+AvishayDev@users.noreply.github.com>
2021-03-21 17:01:33 +0200

    little fix

--- Encryption method/Source.cpp
+++ Encryption method/Source.cpp
@@ -4,6 +4,7 @@
 #include<time.h>
 using namespace std;
 
+//const numbers for good understanding
 int const MIN_CODE = 32;
 int const z_ASCII = 122;
 int const MAX_CODE = 91;
@@ -14,41 +15,49 @@ int main()
 {
 	string TextCoded;
 	char handle;
-	int choose = 1;
+	int choose;
 	fstream fileCode;
+	//the file with the text to code/decode
 	fileCode.open("CodeText.txt");
 	
+	//key for code/decode
 	cout << "Choose Code & deCode Key:\n";
 	cin >> choose;
 	srand(choose);
-
+	
+	//choose code or decode for the text
+	//*can be auto for sender and getter
 	cout << "Choose 1 for code\nChoose 2 for deCode\n";
-	cin >> choose;
-
-	switch (choose)
+	choose = 0;
+	while( choose < 1 || choose > 2 )
 	{
-	case 1:
-		fileCode.get(handle);
-		while (!fileCode.eof())
-		{
-			TextCoded += code(handle, rand() % MAX_CODE);
-			fileCode.get(handle);
-		}
-		break;
-	case 2:
-		fileCode.get(handle);
-		while (!fileCode.eof())
-		{
-			TextCoded += deCode(handle, rand() % MAX_CODE);
-			fileCode.get(handle);
-		}
-		break;
-	default:
-		cout << "Please Choose only 1 or 2!\n";
 		cin >> choose;
-		break;
+		switch (choose)
+		{
+		case 1:
+			//read all chars from file and code them
+			fileCode.get(handle);
+			while (!fileCode.eof())
+			{
+				TextCoded += code(handle, rand() % MAX_CODE);
+				fileCode.get(handle);
+			}
+			break;
+		case 2:
+			//read all chars from file and decode them
+			fileCode.get(handle);
+			while (!fileCode.eof())
+			{
+				TextCoded += deCode(handle, rand() % MAX_CODE);
+				fileCode.get(handle);
+			}
+			break;
+		default:
+			cout << "Please Choose only 1 or 2!\n";
+			cin >> choose;
+			break;
+		}
 	}
-
 	fileCode.close();
 	fileCode.open("CodeText.txt");
 	fileCode << TextCoded;
